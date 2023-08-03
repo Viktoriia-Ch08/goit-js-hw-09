fd39b9f2e3d18836298df4b8d6c02d00738a4643
Viktoriia Cherkashyna <vikushacherk98@gmail.com>
2023-08-03 12:26:18 +0300

    added color-switcher functions

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
+const refs = {
+  bodyEl: document.querySelector('body'),
+  startBtn: document.querySelector('button[data-start]'),
+  stopBtn: document.querySelector('button[data-stop]'),
+};
+let clickOnBtn = null;
 
+refs.startBtn.addEventListener('click', onClick);
+refs.stopBtn.addEventListener('click', onStop);
+
+function onClick() {
+  clickOnBtn = setInterval(() => {
+    refs.bodyEl.style.backgroundColor = getRandomHexColor();
+    refs.startBtn.disabled = true;
+    refs.stopBtn.disabled = false;
+  }, 1000);
+}
+
+function onStop() {
+  clearInterval(clickOnBtn);
+  refs.stopBtn.disabled = true;
+  refs.startBtn.disabled = false;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
