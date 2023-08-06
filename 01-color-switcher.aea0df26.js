e3d3df06340c3d8d2f692bcd311657b61e0e317f
Viktoriia-Ch08 <112558987+Viktoriia-Ch08@users.noreply.github.com>
2023-08-06 16:12:06 +0000

    Deploying to gh-pages from @ Viktoriia-Ch08/goit-js-hw-09@5a88ffc9bec290d228ea2e999ca9baefded7406f 🚀

---
+++ 01-color-switcher.aea0df26.js
@@ -0,0 +1,2 @@
+const t={bodyEl:document.querySelector("body"),startBtn:document.querySelector("button[data-start]"),stopBtn:document.querySelector("button[data-stop]")};let n=null;function e(n){t.startBtn.disabled=n,t.stopBtn.disabled=!n}t.stopBtn.disabled=!0,t.startBtn.addEventListener("click",(function(){e(!0),n=setInterval((()=>{t.bodyEl.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3)})),t.stopBtn.addEventListener("click",(function(){clearInterval(n),e(!1)}));
+//# sourceMappingURL=01-color-switcher.aea0df26.js.map
